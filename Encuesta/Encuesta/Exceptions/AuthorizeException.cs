5b576254a87b8ab761cbf8c9b3e6418b088679ac
danilo.itzep <danilo.itzep-ext@dominion-global.com>
2021-10-10 23:27:05 -0600

    Excepciones personalizadas
    Se agregan excepciones personalizadas

---
+++ Encuesta/Encuesta/Exceptions/AuthorizeException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Encuesta.Exceptions
+{
+    public class AuthorizeException : Exception
+    {
+        public AuthorizeException()
+        {
+
+        }
+
+        public AuthorizeException(string message) : base(message)
+        {
+
+        }
+    }
+}
---
+++ Encuesta/Encuesta/Exceptions/GlobalExceptionFilter.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System.Net;
+
+namespace Encuesta.Exceptions
+{
+    public class GlobalExceptionFilter : IExceptionFilter
+    {
+        public void OnException(ExceptionContext context)
+        {
+            if (context.Exception.GetType() == typeof(BusinessException))
+            {
+                var exception = (BusinessException)context.Exception;
+                var validation = new
+                {
+                    Status = 400,
+                    Title = "Bad Request",
+                    Detail = exception.Message
+                };
+
+                var json = new
+                {
+                    errors = new[] { validation }
+                };
+
+                context.Result = new BadRequestObjectResult(json);
+                context.HttpContext.Response.StatusCode = (int)HttpStatusCode.BadRequest;
+                context.ExceptionHandled = true;
+            }
+
+            if (context.Exception.GetType() == typeof(AuthorizeException))
+            {
+                var exception = (AuthorizeException)context.Exception;
+                var validation = new
+                {
+                    Status = 403,
+                    Title = "Forbidden",
+                    Detail = exception.Message
+                };
+
+                var json = new
+                {
+                    errors = new[] { validation }
+                };
+
+                context.Result = new BadRequestObjectResult(json);
+                context.HttpContext.Response.StatusCode = (int)HttpStatusCode.Forbidden;
+                context.ExceptionHandled = true;
+            }
+        }
+    }
+}
